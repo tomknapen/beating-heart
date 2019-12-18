37cb462f1313b8afc5eadf282f32d7da128594d4
Tom <tom.knapen@kbc.be>
2019-12-18 21:48:36 +0100

    node-red docker

--- node-red/settings.js
+++ node-red/settings.js
@@ -101,7 +101,7 @@ module.exports = {
         users: [{
             username: process.env.USERNAME,
             password: process.env.PASSWORD,
-            permissions: "*"
+            permissions: "*" 
         }]
     },
 
